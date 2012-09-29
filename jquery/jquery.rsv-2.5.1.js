90037e0d9e37d663e6ec1380d76f8f28abb47b5b
Siwei Shen <sg552sg552@gmail.com>
2012-09-29 10:35:14 +0800

    fixed bug: once the form is validated, should hide the previous error message div and clean the errorFieldClass

--- jquery/jquery.rsv-2.5.1.js
+++ jquery/jquery.rsv-2.5.1.js
@@ -50,10 +50,19 @@
    */
   $.fn.RSV.validate = function(event)
   {
+
     options = event.data.options;
     var form  = event.data.currForm;
     var rules = options.rules;
     returnHash = [];
+
+    //hide the previous error message if there is in the displayType: 'display-html'.
+    if(options.displayType == "display-html"){
+        $("#"+options.errorTargetElementId).text("").css("display", "none")
+        if(options.errorFieldClass != null ) {
+            $("."+options.errorFieldClass).removeClass(options.errorFieldClass)
+        }
+    }
 
     // loop through rules
     for (var i=0; i<rules.length; i++)
